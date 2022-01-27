51c178f7bb8972817c83828812d017654dac59e0
Frédéric NOËL <contact@fredericnoel.com>
2022-01-27 16:46:54 +0100

    Appel fonction inclusionInc()

--- includes/main.php
+++ includes/main.php
@@ -2,4 +2,6 @@
 require_once './includes/nav.php';
 ?>
 <h1>GameLib</h1>
-<p>Narwhal gentrify tbh 90's. Master cleanse helvetica XOXO man bun. Poke tbh snackwave meggings everyday carry 90's typewriter crucifix intelligentsia bushwick selvage drinking vinegar aesthetic jianbing. Wayfarers flexitarian before they sold out cliche gochujang hoodie.</p>
+<?php
+
+inclusionInc('home');
