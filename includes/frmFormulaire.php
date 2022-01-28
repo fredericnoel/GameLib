bd1133304b64ca40e435a38c511b8f826a6fe75a
Frédéric NOËL <contact@fredericnoel.com>
2022-01-28 11:46:37 +0100

    Formulaire test HTML

---
+++ includes/frmFormulaire.php
@@ -0,0 +1,8 @@
+<form action="index.php?page=formulaire" method="post">
+    <label for="nom">Nom :</label><input type="text" id="nom" name="nom" /><br />
+    <label for="prenom">Prénom :</label><input type="text" id="prenom" name="prenom" /><br />
+    <label for="email">e-mail :</label><input type="text" id="email" name="email" /><br />
+    <input type="reset" value="Effacer" />
+    <input type="submit" value="Clique-moi grand fou !" />
+    <input type="hidden" name="frm" />
+</form>
