1853c7b456db0e67f3f043c70b9ad0171f7e6f7f
Frédéric NOËL <contact@fredericnoel.com>
2022-02-01 19:36:30 +0100

    Ajout de la fonction htmlentities()

--- includes/formulaire.inc.php
+++ includes/formulaire.inc.php
@@ -1,8 +1,58 @@
 <?php
 if (isset($_POST['frm'])) {
-    echo "Je viens du formulaire !";
+    $nom = htmlentities(trim($_POST['nom'])) ?? '';
+    $prenom = htmlentities(trim($_POST['prenom'])) ?? '';
+    $email = htmlentities(trim($_POST['email'])) ?? '';
+    $password = htmlentities(trim($_POST['password'])) ?? '';
+    $passwordverif = htmlentities(trim($_POST['passwordverif'])) ?? '';
+
+    $erreur = array();
+
+    if (strlen($nom) === 0)
+        array_push($erreur, "Veuillez saisir votre nom");
+
+    elseif (!ctype_alpha($nom))
+        array_push($erreur, "Veuillez saisir des caractères alphabétiques");
+
+    if (strlen($prenom) === 0)
+        array_push($erreur, "Veuillez saisir votre prénom");
+
+    elseif (!ctype_alpha($prenom))
+        array_push($erreur, "Veuillez saisir des caractères alphabétiques");
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        array_push($erreur, "Veuillez saisir un e-mail valide");
+
+    if (strlen($password) === 0)
+        array_push($erreur, "Veuillez saisir un mot de passe");
+
+    if (strlen($passwordverif) === 0)
+        array_push($erreur, "Veuillez saisir la vérification de votre mot de passe");
+
+    if ($password !== $passwordverif)
+        array_push($erreur, "Vos mots de passe ne correspondent pas");
+
+    if (count($erreur) === 0) {
+        echo "Traitement du formulaire";
+    } else {
+        $messageErreur = "<ul>";
+        $i = 0;
+        do {
+            $messageErreur .= "<li>";
+            $messageErreur .= $erreur[$i];
+            $messageErreur .= "</li>";
+            $i++;
+        } while ($i < count($erreur));
+
+        $messageErreur .= "</ul>";
+
+        echo $messageErreur;
+
+        echo password_hash($password,  PASSWORD_BCRYPT);
+    }
 } else {
-    echo "Je viens du futur !";
+    echo "Merci de renseigner le formulaire";
+    $nom = $prenom = $email = '';
 }
 
 
