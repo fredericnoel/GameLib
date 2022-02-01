de350262422f9fcce522210e8af26c9151196ac4
Frédéric NOËL <contact@fredericnoel.com>
2022-02-01 19:36:52 +0100

    Ajout d'un champs password et vérification password

--- includes/frmFormulaire.php
+++ includes/frmFormulaire.php
@@ -1,7 +1,9 @@
 <form action="index.php?page=formulaire" method="post">
-    <label for="nom">Nom :</label><input type="text" id="nom" name="nom" /><br />
-    <label for="prenom">Prénom :</label><input type="text" id="prenom" name="prenom" /><br />
-    <label for="email">e-mail :</label><input type="text" id="email" name="email" /><br />
+    <label for="nom">Nom :</label><input type="text" id="nom" name="nom" value="<?php echo $nom;?>" /><br />
+    <label for="prenom">Prénom :</label><input type="text" id="prenom" name="prenom"  value="<?php echo $prenom;?>" /><br />
+    <label for="email">e-mail :</label><input type="text" id="email" name="email"  value="<?php echo $email;?>" /><br />
+    <label for="password">Mot de passe :</label><input type="password" id="password" name="password" /><br />
+    <label for="passwordverif">Vérification mot de passe :</label><input type="password" id="passwordverif" name="passwordverif" /><br />
     <input type="reset" value="Effacer" />
     <input type="submit" value="Clique-moi grand fou !" />
     <input type="hidden" name="frm" />
