c4986c43f07413ab7cdcd36996d8213f8c320487
Frédéric NOËL <contact@fredericnoel.com>
2022-02-02 10:39:30 +0100

    SPL autoload classes

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 date_default_timezone_set('Europe/Paris');
 // setlocale(LC_ALL, ''); spécifique Windows
 
+spl_autoload_register(function ($className) {
+    include './classes/' . $className . '.php';
+});
+
 require_once './functions/autoLoadFunction.php';
 require_once './includes/head.php';
 require_once './includes/main.php';
