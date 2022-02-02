e2ed7271ceb4d5faf8c9b246e19abd8baae18a99
Frédéric NOËL <contact@fredericnoel.com>
2022-02-02 13:28:17 +0100

    Test instance classe Sql

--- index.php
+++ index.php
@@ -10,3 +10,5 @@ require_once './functions/autoLoadFunction.php';
 require_once './includes/head.php';
 require_once './includes/main.php';
 require_once './includes/footer.php';
+
+$toto = new Sql;
