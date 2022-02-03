03cda47fca7c85ed797b034a9f41bff2a4561e9a
Frédéric NOËL <contact@fredericnoel.com>
2022-02-03 15:10:00 +0100

    Formulaire HTML login

---
+++ includes/frmLogin.php
@@ -0,0 +1,6 @@
+<form action="index.php?page=login">
+    <label for="mail">e-mail :</label><input type="text" id="mail" name="mail" />
+    <label for="mdp">Mot de passe</label><input type="password" id="mdp" name="mdp" />
+    <input type="reset" value="Effacer" />
+    <input type="submit" value="Envoyer" name="envoi" />
+</form>
