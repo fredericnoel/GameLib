0108e8bec87731311ea7c6634fda4504c826688e
Frédéric NOËL <contact@fredericnoel.com>
2022-02-03 15:16:27 +0100

    Corrections mineures

--- includes/frmLogin.php
+++ includes/frmLogin.php
@@ -1,4 +1,4 @@
-<form action="index.php?page=login">
+<form action="index.php?page=login" method="post">
     <label for="mail">e-mail :</label><input type="text" id="mail" name="mail" />
     <label for="mdp">Mot de passe</label><input type="password" id="mdp" name="mdp" />
     <input type="reset" value="Effacer" />
