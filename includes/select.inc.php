0475f9ec5566c287ae6a8282aee0e475480040d7
Frédéric NOËL <contact@fredericnoel.com>
2022-02-03 13:32:58 +0100

    Requête récupération utilisateurs
    Affichage sous forme de tableau HTML

--- includes/select.inc.php
+++ includes/select.inc.php
@@ -9,11 +9,42 @@ try{
     $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $requete = $conn->prepare("SELECT * FROM utilisateurs");
+    $requete = $conn->prepare("SELECT * FROM utilisateurs ORDER BY nom ASC");
     $requete->execute();
     $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-    dump($resultat);
+    $html = "<table>";
+    $html .= "<tr>";
+    $html .= "<th>ID utilisateur</th>";
+    $html .= "<th>Nom</th>";
+    $html .= "<th>Prenom</th>";
+    $html .= "<th>Mail</th>";
+    $html .= "<th>Date inscription</th>";
+    $html .= "</tr>";
+
+    for($i = 0 ; $i < count($resultat) ; $i++) {
+        $html .= "<tr>";
+        
+        foreach($resultat[$i] as $key=>$value) {
+            if($key != "mdp") {
+                $html .= "<td>";
+                if($key == "dateinscription") {
+                    $date = date_create($value);
+                    $html .= $date->format('d/m/Y H:i:s');
+                } 
+                else {
+                    $html .= $value;
+                }
+                $html .= "</td>";
+            }   
+        }
+
+        $html .= "</tr>";
+    }
+
+    $html .= "</table>";
+
+    echo $html;
 }
 
 catch(PDOException $e){
