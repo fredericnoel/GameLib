429720142abb25183f63311941e7bf00225fa73c
Frédéric NOËL <contact@fredericnoel.com>
2022-02-04 11:34:59 +0100

    Affochage prénom et nom utilisateur loggé

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -1,1 +1,11 @@
 <h1>Home</h1>
+<?php
+if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
+    $bienvenue = "<p>";
+    $bienvenue .= "Bonjour ";
+    $bienvenue .= $_SESSION['prenom'];
+    $bienvenue .= " ";
+    $bienvenue .= $_SESSION['nom'];
+    $bienvenue .= "</p>";
+    echo $bienvenue;
+}
