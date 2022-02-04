0b9c6e9aec46f8766d14d6dbf5ac99ad08c02716
Frédéric NOËL <contact@fredericnoel.com>
2022-02-04 11:33:31 +0100

    Ajout login/logout dans le menu

--- includes/nav.php
+++ includes/nav.php
@@ -3,6 +3,12 @@
         <ul>
             <li><a href="index.php?page=home">Home</a></li>
             <li><a href="index.php?page=news">News</a></li>
+            <?php
+                if(isset($_SESSION['login']) && $_SESSION['login'] === true)
+                    echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+                else
+                    echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+            ?>
             <li><a href="index.php?page=contact">Contact</a></li>
         </ul>
     </nav>
