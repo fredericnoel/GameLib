0dd93243ab85979a245f0bd609a1aa95954c1d4f
Frédéric NOËL <contact@fredericnoel.com>
2022-02-07 16:56:01 +0100

    Modification setlocale()

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once './functions/autoLoadFunction.php';
 session_start();
 date_default_timezone_set('Europe/Paris');
-setlocale(LC_ALL, 'fr_FR');
+setlocale(LC_ALL, 'fr_FR', 'fr', 'FR', 'fr_FR@euro');
 
 spl_autoload_register(function ($className) {
     include './classes/' . $className . '.php';
