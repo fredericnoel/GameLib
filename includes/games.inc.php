9f8ad647078b019adc3f3ecc63cb6d2d0b4ed83a
Nouira Alim <nouiraalim@gmail.com>
2022-02-08 16:49:18 +0100

    Insertion des jeux en BDD depuis le formulaire

--- includes/games.inc.php
+++ includes/games.inc.php
@@ -3,14 +3,21 @@
 
 dump($_POST);
 
-if (isset($_POST['inscription'])) {
-    $title = htmlentities(ucfirst(mb_strtolower(trim($_POST['title'])))) ?? '';
+if (isset($_POST['validation'])) {
+    $titleWords = explode(" ", mb_strtolower(trim($_POST['title']))  ?? '');
+    $title = "";
+    for ($cnt=0; $cnt < count($titleWords); $cnt++) { 
+        $titleWords[$cnt] = ucfirst($titleWords[$cnt]);
+        $title .= " " .$titleWords[$cnt];
+    }
+    
+    $title = htmlentities($title);
     $releaseDate = trim($_POST['releaseDate']) ?? '';
     $description = htmlentities(trim($_POST['description'])) ?? '';
 
     $erreur = array();
 
-    if (preg_match('/(*UTF8)^[[:alpha:]]+$/', html_entity_decode($title)) !== 1)
+    if (strlen($title) === 0)
         array_push($erreur, "Veuillez saisir un titre");
     else
         $title = html_entity_decode($title);
@@ -22,7 +29,41 @@ if (isset($_POST['inscription'])) {
         array_push($erreur, "Veuillez saisir une description");
 
     if (count($erreur) === 0) {
+        $serverName = "localhost";
+        $userName = "root";
+        $database = "gamelib";
+        $userPassword = "";
 
+        try {
+            $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $requete = $conn->prepare("SELECT * FROM games WHERE title='$title'");
+            $requete->execute();
+            $resultat = $requete->fetchAll(PDO::FETCH_OBJ);
+           
+            if(count($resultat) !== 0) {
+                echo "<p>Le titre du jeu est déjà enregistrée dans la base de données</p>";
+            }
+
+            else {
+                $query = $conn->prepare("
+                INSERT INTO games(title, releasedate, description)
+                VALUES (:title, :releaseDate, :description)
+                ");
+
+                $query->bindParam(':title', $title, PDO::PARAM_STR_CHAR);
+                $query->bindParam(':releaseDate', $releaseDate);
+                $query->bindParam(':description', $description, PDO::PARAM_STR_CHAR);
+                $query->execute();
+                
+                echo "<p>Insertions effectuées</p>";
+            }
+        } catch (PDOException $e) {
+            die("Erreur :  " . $e->getMessage());
+        }
+
+        $conn = null;
     } else {
         $messageErreur = "<ul>";
         $i = 0;
