277ba5be45bb433da4ff4a922b8f4a6d5999db1d
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-08 16:31:04 +0100

    Correction studio.inc.php v1.0

--- includes/studio.inc.php
+++ includes/studio.inc.php
@@ -1,13 +1,13 @@
 <h1>Studios</h1>
 <?php
-if (isset($_POST['inscription'])) {
-    $name = mb_strtoupper(trim($_POST['name'])) ?? '';
+if (isset($_POST['studio'])) {
+    $name = htmlentities(mb_strtoupper(trim($_POST['name']))) ?? '';
     $country = htmlentities(ucfirst(mb_strtolower(trim($_POST['country'])))) ?? '';
 
     $erreur = array();
 
     if (preg_match('/(*UTF8)^[[:alpha:]]+$/', html_entity_decode($name)) !== 1)
-        array_push($erreur, "Veuillez saisir le nom du studio");
+        array_push($erreur, "Veuillez saisir Le nom du studio");
     else
         $name = html_entity_decode($name);
 
@@ -16,52 +16,49 @@ if (isset($_POST['inscription'])) {
     else
         $country = html_entity_decode($country);
 
+    if (count($erreur) === 0) {
+        $serverName = "localhost";
+        $userName = "root";
+        $database = "gamelib";
+        $userPassword = "";
 
-        if (count($erreur) === 0) {
-            $serverName = "localhost";
-            $userName = "root";
-            $database = "gamelib";
-            $userPassword = "";
-    
-            try {
-                $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $password = password_hash($password, PASSWORD_DEFAULT);
-    
+        try {
+            $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                  
                 $query = $conn->prepare("
-                    INSERT INTO users(id_studio, name, country)
-                    VALUES (:id, :name, :country)
+                INSERT INTO studios(name, country)
+                VALUES (:name, :country)
                 ");
-    
-                $id = null;
-                $query->bindParam(':id', $id);
-                $query->bindParam(':name', $name);
-                $query->bindParam(':country', $country);
+
+                $query->bindParam(':name', $name, PDO::PARAM_STR_CHAR);
+                $query->bindParam(':country', $country, PDO::PARAM_STR_CHAR);
 
                 $query->execute();
+                
                 echo "<p>Insertions effectuées</p>";
-            } catch (PDOException $e) {
-                die("Erreur :  " . $e->getMessage());
             }
-    
-            $conn = null;
-        } 
-        else {
-            $messageErreur = "<ul>";
-            $i = 0;
-            do {
-                $messageErreur .= "<li>" . $erreur[$i] . "</li>";
-                $i++;
-            } while ($i < count($erreur));
-    
-            $messageErreur .= "</ul>";
-    
-            echo $messageErreur;
+         catch (PDOException $e) {
+            die("Erreur :  " . $e->getMessage());
         }
-    } 
-    else {
-        echo "<h2>Merci de renseigner le formulaire&nbsp;:</h2>";
-        $name = $country =  '';
-    }
 
-require_once './includes/frmStudio.php';
+        $conn = null;
+    } else {
+        $messageErreur = "<ul>";
+        $i = 0;
+        do {
+            $messageErreur .= "<li>" . $erreur[$i] . "</li>";
+            $i++;
+        } while ($i < count($erreur));
+
+        $messageErreur .= "</ul>";
+
+        echo $messageErreur;
+        include 'frmStudio.php';
+    }
+} else {
+    echo "<h2>Merci de renseigner le formulaire&nbsp;:</h2>";
+    $name = $country = '';
+
+    include 'frmStudio.php';
+}
