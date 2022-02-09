5c88c4e85762b303a342e0469706ad252d5e0203
Frédéric NOËL <contact@fredericnoel.com>
2022-02-09 11:48:07 +0100

    Si le champ 'pseudo' est vide, affiche le prénom et le nom

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -3,9 +3,14 @@
 if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
     $bienvenue = "<p>";
     $bienvenue .= "Bonjour ";
-    $bienvenue .= $_SESSION['prenom'];
-    $bienvenue .= " ";
-    $bienvenue .= $_SESSION['nom'];
+    if ($_SESSION['pseudo'] !== "") {
+        $bienvenue .= $_SESSION['pseudo'];
+    }
+    else {
+        $bienvenue .= $_SESSION['prenom'];
+        $bienvenue .= " ";
+        $bienvenue .= $_SESSION['nom'];
+    }
     $bienvenue .= "</p>";
     echo $bienvenue;
 }
