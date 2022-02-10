a515bc48017a2316ea292a56af2fb6a623893991
Nouira Alim <nouiraalim@gmail.com>
2022-02-10 09:22:52 +0100

    Ajout d'une liste déroulante pour les studios

--- includes/frmGames.php
+++ includes/frmGames.php
@@ -1,17 +1,23 @@
 <form method="post" action="index.php?page=games">
     <ul>
         <li><label for="title"> Titre : </label>
-            <input type="texte" id="title" name="title" value="<?php echo $title;?>"><br></li>
+            <input type="texte" id="title" name="title" value="<?php echo $title;?>"></li>
 
         <li><label for="releaseDate"> Date de sortie : </label><input type="date" id="releaseDate" name="releaseDate"
-                value="<?php echo $releaseDate;?>"><br></li>
+                value="<?php echo $releaseDate;?>"></li>
 
         <li><label for="description"> Description : </label>
-            <textarea id="description" name="description" value="<?php echo $description;?>"></textarea><br></li>
+            <textarea id="description" name="description" value="<?php echo $description;?>"></textarea></li>
+
+        <li><label for="studios"> Studios : </label>
+            <select range="studio" id="studio">
+            <?php echo getStudios();?>
+		    </select>
+
+        </li>
 
         <li><input type="reset" value="Effacer">
             <input type="submit" value="Valider" name="validation"></li>
     </ul>
 </form>
 <a href="index.php?page=library">Voir tous les jeux</a>
-
