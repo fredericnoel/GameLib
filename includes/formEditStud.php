c5bfc036c410b6b7f1cf1ae93276fe02f73ce213
Franck Boirot <franck.brt@hotmail.fr>
2022-02-10 15:31:20 +0100

    form liaison stud
    edit

--- includes/formEditStud.php
+++ includes/formEditStud.php
@@ -1,59 +1,53 @@
-<?php
-        
+<?php      
     $serverName = "localhost";
     $userName = "root";
     $database = "gamelib";
     $userPassword = "";
 
-    try{
+try{
         $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $requete = $conn->prepare("SELECT * FROM editors ORDER BY name ASC");
         $requete->execute();
         $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
-        // $idEditor = $resultat['id_editor'];
-        // $nameEditor = $resultat['name'];
-        // $nameCountry = $resultat['country'];
 
-        $html = "<form method='post' action='index.php?page=visuediteurs'>";
-        $html .= "<ul>";
-        $html .= "<li><label for='editor'>Editeurs : </label>";
-        $html .= "<select name='editor' id='editor'>";
+        $requete2 = $conn->prepare("SELECT * FROM studios ORDER BY name ASC");
+        $requete2->execute();
+        $resultat2 = $requete2->fetchAll(PDO::FETCH_ASSOC);
 
-
-
-        $html .= "</select></li></ul></form>";
+        $html = "<form action='index.php?page=editeur_studio' method='post'>";
+        $html .="<label for='editor'>Editeur : </label>";
+        $html .="<select name='editor' id='editor'>";
         
-        dump($resultat);
+        for($i = 0 ; $i < count($resultat) ; $i++) {
+            $indexEditeur = $resultat[$i]['id_editor'];
+            $nameEditeur = $resultat[$i]['name'];
 
+            $html .="<option value=$indexEditeur>$nameEditeur</option>";
+           
+        }
+        $html .= "</select>";
+// ///////////////////////////////////////////////////////////////////////////////////
+        $html .="<label for='studio'>Studio : </label>";
+        $html .="<select name='studio' id='studio'>";
+        
+        for($i = 0 ; $i < count($resultat2) ; $i++) {
+            $indexStudio = $resultat2[$i]['id_studio'];
+            $nameStudio = $resultat2[$i]['name'];
 
-                        // $html = "<option value='$idEditor'>$nameEditor ($nameCountry)</option>";
+            $html .="<option value=$indexStudio>$nameStudio</option>";
+           
+        }
+        $html .= "</select>";
 
-                        // for($i = 0 ; $i < count($resultat) ; $i++) { 
-                            // foreach($resultat[$i] as $key=>$value) {
-                            // $html = "<option value='$idEditor'>$nameEditor ($nameCountry)</option>";
-                            // }
-                            // }
-                            echo $html;
-                            }
+        $html .= "<input type='submit' value='Valider' name='linkEditor' />";
+        $html .= "</form>";
+        echo $html;
 
-                            catch(PDOException $e){
-                            die("Erreur : " . $e->getMessage());
-                            }
-
-                            $conn= null;
-
-?>
-
+}
+catch(PDOException $e){
+die("Erreur : " . $e->getMessage());
+}
+$conn= null;
             
-</li>
-        <li><label for="studio">Studio : </label>
-            <select name="studio" id="studio">
-                <option value="France">France </option>
-                <option value="Afghanistan">Afghanistan </option>
-            </select>
-        </li>
-        <li><input type="submit" value="Envoyer" name="envoi" /></li>
-    </ul>
-</form>
